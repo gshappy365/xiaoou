6e8f09ae33e0d87e71d3785a6c24561388104721
gaoshan <gaohappy365@gmail.com>
2015-11-30 18:07:02 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override')
+var AV = require('leanengine');
+
+var users = require('./routes/users');
+var todos = require('./routes/todos');
+var cloud = require('./cloud');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use('/static', express.static('public'));
+
+// 加载云代码方法
+app.use(cloud);
+
+// 加载 cookieSession 以支持 AV.User 的会话状态
+app.use(AV.Cloud.CookieSession({ secret: '05XgTktKPMkU', maxAge: 3600000, fetchUser: true }));
+
+// 强制使用 https
+app.enable('trust proxy');
+app.use(AV.Cloud.HttpsRedirect());
+
+app.use(methodOverride('_method'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 可以将一类的路由单独保存在一个文件中
+app.use('/todos', todos);
+app.use('/users', users);
+
+app.get('/', function(req, res) {
+  res.redirect('/todos');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
---
+++ start.sh
@@ -0,0 +1,6 @@
+export LC_APP_ID=8Y3kh9PnW2D2fx1LVJGQ87k3
+export LC_APP_KEY=E29HOHTLQ7vorYh3x0TLg2Bd
+export LC_APP_MASTER_KEY=BAdTalLM7VCrnUhM6ST8RE5d
+export LC_APP_PORT=3000
+
+supervisor --no-restart-on exit server.js
